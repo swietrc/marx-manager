24905ae87edacabccf07a35eca9bd2d14dc43a36
Simon Wietrich <simon.wietrich@gmail.com>
2017-05-15 21:20:31 +0200

    Working mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,16 @@ Rails.application.configure do
   config.active_record.migration_error = :page_load
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV['MAILER_GMAIL_USERNAME'],
+    password:             ENV['MAILER_GMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
