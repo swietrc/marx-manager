a8af4544f1af4c9d4c2ac59f17cca9d822a6c115
SuperMouette <loic.souverain@gmail.com>
2017-05-19 10:38:57 +0200

    amélioation affichage matiere

--- app/helpers/subjects_helper.rb
+++ app/helpers/subjects_helper.rb
@@ -1,2 +1,13 @@
 module SubjectsHelper
+  def student_avg s
+    avg = 0
+    n  = 0
+    @subject.exams.each do |exam|
+      if (exam.exams_students.where(student: s))
+        avg += exam.exams_students.where(student: s)[0].grade
+        n+=1
+      end
+    end
+    return avg/n
+  end
 end
