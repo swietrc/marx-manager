a1423cdacbb9a2fb783acbd070892c4b23ce0236
SuperMouette <loic.souverain@gmail.com>
2017-05-19 12:35:32 +0200

    amélioration vues student/épreuve

--- app/helpers/subjects_helper.rb
+++ app/helpers/subjects_helper.rb
@@ -1,10 +1,10 @@
 module SubjectsHelper
-  def student_avg s
+  def student_avg(subject,student)
     avg = 0
     n  = 0
-    @subject.exams.each do |exam|
-      if (exam.exams_students.where(student: s))
-        avg += exam.exams_students.where(student: s)[0].grade
+    subject.exams.each do |exam|
+      if (exam.exams_students.where(student: student))
+        avg += exam.exams_students.where(student: student)[0].grade
         n+=1
       end
     end
