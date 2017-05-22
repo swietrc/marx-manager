1719312927ea179482475618539272db8dcaad36
swietrich <swietrich@etu.unistra.fr>
2017-05-22 11:42:07 +0200

    Fix migrate bug

--- db/migrate/20170428142433_devise_create_users.rb
+++ db/migrate/20170428142433_devise_create_users.rb
@@ -38,11 +38,5 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
-    
-    User.create! do |u|
-        u.email = 'test@test.com'
-        u.password = 'testest'
-        u.approved = true
-    end
   end
 end
